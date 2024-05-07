63d45eeecb962a755eb6887cd7187e7d28c713d5
Oliver Sharif <spockmob@gmail.com>
2024-05-07 17:10:41 +0200

    Add setup.py and __init__.py for package initialization
    This commit introduces setup.py, which contains important package information and dependencies. Additionally, an __init__.py file was added that declares the version and author of the package. The setup.py file will ease installation, dependencies control, and package distribution in the future.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='vreedb-python',
+    version='0.0.1',
+    description='the vreedb python client',
+    url='https://github.com/spookieoli/vreedb-python',
+    author='Oliver Sharif',
+    author_email='Oliver.Sharif@protonmail.com',
+    license='apache 2',
+    packages=['vreedb'],
+    install_requires=['httpx>=0.27.0'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Everyone',
+        'License :: OSI Approved :: Apache 2 License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MS :: Windows',
+        'Programming Language :: Python :: 3.9+',
+    ],
+)
---
+++ vreedb/__init__.py
@@ -0,0 +1,8 @@
+"""
+vreedb-python
+
+The python Vreedb Client
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Oliver Sharif'
